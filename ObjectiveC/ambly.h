db264a05688fb70ab31dca76117c91735241bb41
Zhao Wang <zhao@playchat.net>
2016-03-17 23:11:45 -0700

    Carthage support first step

---
+++ ObjectiveC/ambly.h
@@ -0,0 +1,21 @@
+//
+//  ambly.h
+//  ambly
+//
+//  Created by Zhao Wang on 3/17/16.
+//  Copyright © 2016 Mike Fikes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ambly.
+FOUNDATION_EXPORT double amblyVersionNumber;
+
+//! Project version string for ambly.
+FOUNDATION_EXPORT const unsigned char amblyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ambly/PublicHeader.h>
+#import "ABYServer.h"
+#import "ABYUtils.h"
+#import "ABYContextManager.h"
+
